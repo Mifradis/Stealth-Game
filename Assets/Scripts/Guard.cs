2bb3f7802a09aefe9fe955845a15b9d9f3321888
bedirhan çakır <bedirhancakir@windowslive.com>
2023-05-31 16:26:35 +0300

    Player added and LineCast added to guard

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -7,21 +7,64 @@ public class Guard : MonoBehaviour
     public Transform pathHolder;
     public float speed = 8;
     public float waitTime = .3f;
+    public float turnSpeed = 90;
+    public Light spotlight;
+    public LayerMask viewMask;
+    public float viewDistance;
+    float viewAngle;
+    Transform player;
+    Color originalSpotlightColor;
+
     Vector3 targetGizmo;
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        viewAngle = spotlight.spotAngle;
+        originalSpotlightColor = spotlight.color;
         Vector3[] waypoints = new Vector3[pathHolder.childCount];
         for(int i = 0; i < waypoints.Length; i++)
         {
             waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
         }
         StartCoroutine(FollowPath(waypoints));
+    }
+    
+    bool CanSeePlayer()
+    {
+        if(Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            if(angleBetweenGuardAndPlayer  < viewAngle / 2f)
+            {
+                if(!Physics.Linecast(transform.position, player.position, viewMask))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90f - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x)*Mathf.Rad2Deg;
+
+        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+        }
     }
     IEnumerator FollowPath(Vector3[] waypoints)
     {
         transform.position = waypoints[0];
         int targetWaypointIndex = 1;
         Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
         while (true)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
@@ -30,13 +73,22 @@ public class Guard : MonoBehaviour
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetWaypoint = waypoints[targetWaypointIndex];
                 yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
             }
             yield return null;
         }
     }
     private void Update()
     {
-        
+        print(CanSeePlayer());
+        if (CanSeePlayer())
+        {
+            spotlight.color = Color.red;
+        }
+        else
+        {
+            spotlight.color = originalSpotlightColor;
+        }
     }
     private void OnDrawGizmos()
     {
@@ -49,5 +101,7 @@ public class Guard : MonoBehaviour
             previousPosition = waypoint.position;
         }
         Gizmos.DrawLine(previousPosition, startPosition);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
     }
 }
